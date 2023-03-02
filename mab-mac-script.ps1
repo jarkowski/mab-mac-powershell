6fcc7a169ad9651cd0ca9952d44fa659d1566a02
David Jarkowski <4302314+jarkowski@users.noreply.github.com>
2023-03-02 19:42:32 +0100

    Format

--- mab-mac-script.ps1
+++ mab-mac-script.ps1
@@ -14,11 +14,11 @@ $mabDenyLogonGroup = $config.mabDenyLogonGroup
 
 # Get MAC address from user input and format it
 
-    Write-Host " "
-    Write-Host "Provide a MAC address in any format you like"
-    Write-Host "Special charcters - and : will be removed"
-    Write-Host "All letters will be converted to upper case letters"
-    Write-Host " "
+Write-Host " "
+Write-Host "Provide a MAC address in any format you like"
+Write-Host "Special charcters - and : will be removed"
+Write-Host "All letters will be converted to upper case letters"
+Write-Host " "
 
 do {
 
@@ -26,21 +26,23 @@ do {
     $mac = $mac.Replace("-", "").Replace(":", "").Trim().ToUpper()
     if ($mac -match "^[0-9A-F]{12}$") {
         break
-    } else {
+    }
+    else {
         Write-Host "Invalid MAC address. Please try again."
     }
 } while ($true)
 
 # Check if MAC user already exists
 
-$existingUser = Get-ADUser -Filter {Name -like $mac} -SearchBase $mabUserOU -ErrorAction SilentlyContinue
+$existingUser = Get-ADUser -Filter { Name -like $mac } -SearchBase $mabUserOU -ErrorAction SilentlyContinue
 if ($existingUser) {
     # User already exists
     $delete = Read-Host "$mac already exists. Do you want to delete the user object from active directory? (Y/N)"
     if ($delete -eq "Y") {
         Remove-ADUser -Identity $existingUser -Confirm:$false
         Write-Host "User deleted from active directory"
-    } else {
+    }
+    else {
         Write-Host "User not deleted - No changes made."
     }
 } 
@@ -48,35 +50,38 @@ if ($existingUser) {
 else {
     
 
-# Check for available OUs below mabUserOU
-try {
-$mabSubOUs = Get-ADOrganizationalUnit -SearchScope OneLevel -SearchBase "$mabUserOU" -Filter * | Select-Object Name, DistinguishedName
-} catch {
-Write-Host "Can't list child OUs."
-Exit 1
-}
+    # Check for available OUs below mabUserOU
+    try {
+        $mabSubOUs = Get-ADOrganizationalUnit -SearchScope OneLevel -SearchBase "$mabUserOU" -Filter * | Select-Object Name, DistinguishedName
+    }
+    catch {
+        Write-Host "Can't list child OUs."
+        Exit 1
+    }
 
-if ($mabSubOUs) {
-    # Prompt user to select an OU to create the new user in
-    Write-Host "The following sub OUs are available under $mabUserOU "
-    $mabSubOUs | Format-Table -AutoSize
-    do {
-        $selectedOU = Read-Host "Please enter the name of the sub OU to create the new user in"
-        if ($selectedOU -in $mabSubOUs.Name) {
-            $targetOU = "OU=" + $selectedOU + "," + $mabUserOU
-            break
-        } else {
-            Write-Host "Invalid selection. Please try again."
-        }
-    } while ($true)
-} else {
-    # No child OUs found under mabUserOU, exit script
-    Write-Host "No child OUs found under $mabUserOU. Exiting script."
-    Exit
-}
+    if ($mabSubOUs) {
+        # Prompt user to select an OU to create the new user in
+        Write-Host "The following sub OUs are available under $mabUserOU "
+        $mabSubOUs | Format-Table -AutoSize
+        do {
+            $selectedOU = Read-Host "Please enter the name of the sub OU to create the new user in"
+            if ($selectedOU -in $mabSubOUs.Name) {
+                $targetOU = "OU=" + $selectedOU + "," + $mabUserOU
+                break
+            }
+            else {
+                Write-Host "Invalid selection. Please try again."
+            }
+        } while ($true)
+    }
+    else {
+        # No child OUs found under mabUserOU, exit script
+        Write-Host "No child OUs found under $mabUserOU. Exiting script."
+        Exit
+    }
 
-Write-Host "This is the OU that will be used: "
-Write-Host $targetOU
+    Write-Host "This is the OU that will be used: "
+    Write-Host $targetOU
         
     # User does not exist, create new user
     Write-Host "Creating new user for MAC address $mac"
@@ -86,22 +91,23 @@ Write-Host $targetOU
     
     # Create new user with random password
     $userParams = @{
-        Name = $mac
-        SamAccountName = $mac
-        UserPrincipalName = $mac + "@" + $domain
-        AccountPassword = $password
-        PasswordNeverExpires = $true
-        PasswordNotRequired = $false
-        CannotChangePassword = $true
+        Name                              = $mac
+        SamAccountName                    = $mac
+        UserPrincipalName                 = $mac + "@" + $domain
+        AccountPassword                   = $password
+        PasswordNeverExpires              = $true
+        PasswordNotRequired               = $false
+        CannotChangePassword              = $true
         AllowReversiblePasswordEncryption = $true
-        Enabled = $true
-        Path = $targetOU
+        Enabled                           = $true
+        Path                              = $targetOU
     }
     Write-Host "UserParams set."
 
     try {
         $newUser = New-ADUser @userParams -ErrorAction Stop
-    } catch {
+    }
+    catch {
         Write-Host "Error creating user: $_"
         exit 1
     }
@@ -117,7 +123,7 @@ Write-Host $targetOU
     $group = Get-ADGroup $mabDenyLogonGroup -Properties PrimaryGroupToken
     $primaryGroupToken = $group.PrimaryGroupToken
     Write-Host "The Token of group $mabDenyLogonGroup is $primaryGroupToken"
-    Set-aduser -identity $mac -Replace @{PrimaryGroupID=$primaryGroupToken}
+    Set-aduser -identity $mac -Replace @{PrimaryGroupID = $primaryGroupToken }
 
     # Check for success. If the primary group is not set to the DenyLogonGroup,
     # the script must stop BEFORE setting the password to match the username.
@@ -125,11 +131,11 @@ Write-Host $targetOU
     $id = $user.PrimaryGroupID 
     Write-Host "The users primary group ID is $id." 
     if ($user.PrimaryGroupID -eq $primaryGroupToken) {
-    Write-Host "OK, primary group is set correctly."
+        Write-Host "OK, primary group is set correctly."
     } 
     else {
-    Write-Host "Error: PrimaryGroup does not match, stopping"
-    Exit
+        Write-Host "Error: PrimaryGroup does not match, stopping"
+        Exit
     }
 
     # Set password to match username
@@ -151,28 +157,30 @@ Write-Host $targetOU
     # Remove all groups except for the MAB Deny Logon group
     foreach ($group in $groups) {
 
-    if ($group.ToString() -ne $mabDenyLogonGroup.ToString()) {
-        Write-Host "Removing Group: $group"
-        Remove-ADPrincipalGroupMembership -Identity $mac -MemberOf $group -Confirm:$false
+        if ($group.ToString() -ne $mabDenyLogonGroup.ToString()) {
+            Write-Host "Removing Group: $group"
+            Remove-ADPrincipalGroupMembership -Identity $mac -MemberOf $group -Confirm:$false
 
-    } else {}
-}
+        }
+        else {}
+    }
 
-#Verify the the user is only member in $mabDenyLogonGroup
+    #Verify the the user is only member in $mabDenyLogonGroup
 
-# Retrieve a list of groups the user is a member of
-$groups = Get-ADPrincipalGroupMembership -Identity $mac
+    # Retrieve a list of groups the user is a member of
+    $groups = Get-ADPrincipalGroupMembership -Identity $mac
 
-# Check if the user is only a member of the MAB Deny Logon group
+    # Check if the user is only a member of the MAB Deny Logon group
 
-$numberOfGroups = @($groups).Count
-$shouldBeGroup = $groups.ToString()
-$shouldBeGroupTarget = $mabDenyLogonGroup.ToString()
+    $numberOfGroups = @($groups).Count
+    $shouldBeGroup = $groups.ToString()
+    $shouldBeGroupTarget = $mabDenyLogonGroup.ToString()
 
-if (($numberOfGroups -eq 1) -and ($shouldBeGroup -eq $shouldBeGroupTarget )) {
-    Write-Host "Test Passed: User $mac is only a member of the $mabDenyLogonGroup group"
-} else {
-    Write-Host "Test Failed: User $mac is a member of other groups in addition to $mabDenyLogonGroup"
-}
+    if (($numberOfGroups -eq 1) -and ($shouldBeGroup -eq $shouldBeGroupTarget )) {
+        Write-Host "Test Passed: User $mac is only a member of the $mabDenyLogonGroup group"
+    }
+    else {
+        Write-Host "Test Failed: User $mac is a member of other groups in addition to $mabDenyLogonGroup"
+    }
 }
 Write-Host "End of script"
